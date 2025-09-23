b63849dcc42f10cbc941920ac0827ac46ce4f727
noxium-git <raul2008albert@gmail.com>
2025-09-23 09:05:26 +0000

    exercicios em python

---
+++ cicloElemento.py
@@ -0,0 +1,7 @@
+#2- dada uma lista, elabora um programa que permita com um ciclo, inserir elementos na lista, ate que a tecla Q seja pressionada
+alfabeto = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+for x in alfabeto:
+    print(x)
+    if x == "Q":
+        break
